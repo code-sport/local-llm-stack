caccabbc7cbd9b7354248c96393644a8c6ea1b3b
code.sport <git@codesport.info>
2026-07-07 23:08:39 +0200

    chore: remove django backend

--- backend/judoshiai/wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI config for judoshiai project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "judoshiai.settings")
-application = get_wsgi_application()
